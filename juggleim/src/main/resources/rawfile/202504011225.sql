192a07d19f092b47fc7625a0338f6194278a20bd
yuwnloy <yuwnloyblog@163.com>
2025-04-01 20:32:32 +0800

    add listener

--- juggleim/src/main/resources/rawfile/202504011225.sql
+++ juggleim/src/main/resources/rawfile/202504011225.sql
@@ -1,10 +1,10 @@
-create table message(
+create table messages(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     conver_id VARCHAR(64),
     conver_type SMALLINT default 0,
     client_id VARCHAR(64),
-    sender VARCHAR(64),
-    direction boolean,
+    sender_id VARCHAR(64),
+    direction SMALLINT,
     msg_id VARCHAR(64),
     msg_seq INTEGER default 0,
     msg_index INTEGER,
@@ -14,15 +14,15 @@ create table message(
     ext text,
     state SMALLINT,
     flags INTEGER default 0,
-    has_read boolean,
+    has_read SMALLINT,
     read_count INTEGER default 0,
     member_count INTEGER default 0,
-    is_deleted boolean default 0,
+    is_deleted SMALLINT default 0,
     search_content text,
     mention_info text,
     refer_msg_id VARCHAR(64)
 );
-CREATE UNIQUE INDEX IF NOT EXISTS idx_message ON message(msg_id);
-CREATE UNIQUE INDEX IF NOT EXISTS idx_message_client_id ON message(client_id);
-CREATE INDEX IF NOT EXISTS idx_message_conver ON message(conver_type,conver_id);
-CREATE INDEX IF NOT EXISTS idx_message_conver_ts ON message(conver_type,conver_id,sent_time);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_messages ON messages(msg_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_messages_client_id ON messages(client_id);
+CREATE INDEX IF NOT EXISTS idx_messages_conver ON messages(conver_type,conver_id);
+CREATE INDEX IF NOT EXISTS idx_messages_conver_ts ON messages(conver_type,conver_id,sent_time);
